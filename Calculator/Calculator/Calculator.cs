875067e7eeb608ad7a54a713f9ab7449b7dbf9da
shenjintong <shenjintong@gridsum.com>
2015-08-31 14:57:11 +0800

    添加乘法和除法

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+1.	namespace Calculator
+2.	{
+3.	    public class Calculator
+4.	    {
+5.	        public static int Multiply(int nubmer1, int number2)
+6.	        {
+7.	            return nubmer1 * number2;
+8.	        }
+9.	        public static int Division(int number1, int nubmer2)
+10.	        {
+11.	            return number1 / nubmer2;
+12.	        }
+13.	    }
+14.	}
